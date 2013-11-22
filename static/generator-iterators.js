007aebb4e7868b06885b17404ac7ec212e4c1fc4
Erin Swenson-Healey <erin@carbonfive.com>
2013-11-22 09:34:57 -0800

    Fixing Fibonacci algorithm

--- static/generator-iterators.js
+++ static/generator-iterators.js
@@ -1,15 +1,20 @@
 function* fibonacci() {
-  var prev = 0, curr = 1;
+  var a = 0, b = 1, c = 0;
+
   while (true) {
-    prev = curr;
-    curr = prev + curr;
-    yield curr;
+    yield a;
+    c = a;
+    a = b;
+    b = c + b;
   }
 }
 
 var seq = fibonacci();
+log(seq.next().value); // 0
+log(seq.next().value); // 1
 log(seq.next().value); // 1
 log(seq.next().value); // 2
 log(seq.next().value); // 3
 log(seq.next().value); // 5
 log(seq.next().value); // 8
+log(seq.next().value); // 13
