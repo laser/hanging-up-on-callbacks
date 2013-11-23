8c94564648c731a44197f17be5e97a9a6cac5dab
Erin Swenson-Healey <erin@carbonfive.com>
2013-11-22 19:06:00 -0800

    Updating delegation example with comments

--- static/delegation.js
+++ static/delegation.js
@@ -3,15 +3,16 @@ function* add(x,y) {
 }
 
 function* main(){
-  var r = yield add(2,2); // yields a generator of "add"
+  var r = yield add(2, 2); // yield the "add" generator and later serve as entry point when "main" is resumed
   log(r);
 }
 
 function run(){
-  var m      = main();
-  var sub    = m.next().value; // start executing "main" generator, which yields the generator of "add"
-  var result = sub.next(); // start executing "add" generator
-  m.next(result.value); // resume execution of "main" and swap in result yielded from "add"
+  var mainGen   = main(); // get the generator of "main"
+  var addGen    = mainGen.next().value; // start executing "main" generator, which yields the generator of "add"
+  var addResult = addGen.next().value; // start executing "add" generator
+
+  mainGen.next(addResult); // resume execution of "main" and swap in result yielded from "add"
 }
 
 run();
