bfd7611a56cc2dca811daf656c620cc615371aee
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2013-11-23 18:37:29 -0800

    Updating delegation example - it now delegates

--- static/delegation.js
+++ static/delegation.js
@@ -1,18 +1,15 @@
-function* add(x,y) {
-  yield (x + y); // yields the result of x plus y
+function* delegate(x, y) {
+  yield (x + y);
 }
 
-function* main(){
-  var r = yield add(2, 2); // yield the "add" generator and later serve as entry point when "main" is resumed
-  log(r);
+function* delegator(a, b) {
+  yield* delegate(a, b);
 }
 
 function run(){
-  var mainGen   = main(); // get the generator of "main"
-  var addGen    = mainGen.next().value; // start executing "main" generator, which yields the generator of "add"
-  var addResult = addGen.next().value; // start executing "add" generator
-
-  mainGen.next(addResult); // resume execution of "main" and swap in result yielded from "add"
+  var gen       = delegator(5, 1); // get the generator of "delegator"
+  var addResult = gen.next();      // resume generator, which yields result of delegate generator
+  log(addResult.value);            // value is now 6
 }
 
 run();
