27e24ae284ac7bdf8f5a71a69408238080b1a5c8
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-05-22 07:14:15 -0700

    console -> console.log

--- static/src/control-flow.js
+++ static/src/control-flow.js
@@ -4,6 +4,6 @@ function* powGenerator() {
 }
 
 var g = powGenerator();
-log(g.next().value);   // "a", from the first yield
-log(g.next(10).value); // "b", from the second
-log(g.next(2).value);  // 100, the result
+console.log(g.next().value);   // "a", from the first yield
+console.log(g.next(10).value); // "b", from the second
+console.log(g.next(2).value);  // 100, the result
